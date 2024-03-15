de771fa9b7ceddcf798e0072708c78c3bbf14bca
Ruan Massini <ruanmassini@gmail.com>
2024-03-15 13:45:55 -0400

    user's create fixed

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\View\View;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
@@ -15,11 +16,17 @@ class UserController extends Controller
         return view('user.index', compact('findUser'));
     }
 
-    public function create(Request $request): View 
+    public function create(Request $request)
     {
+        $findUser = User::all();
+        
         if($request->isMethod('post')){
             $user = new User();
-            $user->name = $request->nome;
+            $user->name = $request->name;
+            $user->email = $request->email;
+            $user->password = Hash::make($request['password']);
+            $user->save();
+            return redirect()->route('user.index');
         }else{
             return view('user.create');
         }
